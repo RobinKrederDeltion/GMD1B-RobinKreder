70bb57734cd3f0d1d6c73eb82e07fa4127882d96
RobinKrederDeltion <97043715@st.deltion.nl>
2016-11-04 11:01:59 +0100

    Flipperkast project zonder zooi

---
+++ P1/Flipperkast/Assets/Scripts/Deathzone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour {
+
+    public GameManager gamedatascript;
+
+    void OnCollisionEnter(Collision c)
+    {
+        Destroy(c.gameObject);
+        gamedatascript.lives += -1;
+        GameObject.Find("Katapult").GetComponent<Launcher>().activeball = false;
+    }
+}
---
+++ P1/Flipperkast/Assets/Scripts/Launcher.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour
+{
+    public float power;
+    public GameObject prefab;
+    public GameObject pinballSpawn;
+    public bool activeball;
+
+    void SpawnPinball()
+    {
+        GameObject pinball = (GameObject)Instantiate(prefab, pinballSpawn.GetComponent<Transform>().position, pinballSpawn.GetComponent<Transform>().rotation);
+        pinball.GetComponent<Rigidbody>().velocity = pinballSpawn.GetComponent<Transform>().transform.forward * power;
+    }
+
+
+    void Update()
+    
+       
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                if (GameObject.Find("GameManager").GetComponent<GameManager>().lives > 0)
+                {
+                    if (activeball == false)
+                    {
+                        SpawnPinball();
+                    }
+                }
+            activeball = true;
+            }
+        
+        
+    }
+
+
+    
+}
