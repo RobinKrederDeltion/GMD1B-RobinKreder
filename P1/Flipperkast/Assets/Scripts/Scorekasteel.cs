6d1f9eaf90b9ab4ead6e7df02ff4b7c6b4797b31
RobinKrederDeltion <97043715@st.deltion.nl>
2017-01-26 19:39:52 +0100

    Nieuwe Flipperkast
    Hopelijk is alles goed nu :)

---
+++ P1/Flipperkast/Assets/Scripts/Scorekasteel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorekasteel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter(Collision Collision)
+    {
+
+
+        GameObject.Find("GameManager").GetComponent<GameData>().score += 30;
+
+    }
+}
