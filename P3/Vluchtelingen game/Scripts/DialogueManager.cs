9ed46b7788a1582210f3c3d6d7f78f869b0a3233
RobinKrederDeltion <97043715@st.deltion.nl>
2017-04-14 22:57:35 +0100

    Scripts voor frans
    Periode 3, Vluchtelingen

---
+++ P3/Vluchtelingen game/Scripts/DialogueManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public float length;
+    public RaycastHit hit;
+    public GameObject cam;
+    public GameObject panel;
+    public GameObject canvas;
+    public bool convo;
+    public int stringNumber;
+    public Transform player;
+   
+    //panel wordt bij start automatisch uit gezet
+    void Start()
+    {
+        panel.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        //Wanneer de Raycast collide met een object met de tag "NPC" en op Activate "E" drukt, dan wordt het tekstvakje active en gaat convo op true, als je dat niet doet is alles uit.
+        Debug.DrawRay(cam.transform.position, cam.transform.forward * length, Color.blue);
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, length))   
+        {
+            if (Input.GetButtonDown("Activate"))
+            {
+                if (hit.collider.tag == ("NPC"))
+                {
+                    panel.SetActive(true);
+                    convo = true;
+                }
+            }
+        }
+
+        if (convo == true)
+        {
+            InConversation();
+        }
+        
+    }
+    //Als er ja wordt gezegd dan wordt de stringnummer +1 en bij nee +3
+    public void AnswerYes()
+    {
+        stringNumber += 1;
+        hit.collider.GetComponent<Dialogue>().EventCheck();
+    }
+
+    public void AnswerNo()
+    {
+        stringNumber += 3;
+        hit.collider.GetComponent<Dialogue>().EventCheck();
+    }
+
+    //hierbij wordt gecontroleerd of de int "stringNumber" gelijk of onder de lis is, anders is de panel en convo uit.
+    void InConversation()
+    {
+        if (stringNumber < hit.collider.GetComponent<Dialogue>().conversation.Count)
+        {
+            canvas.GetComponent<UIManager>().actieveString = hit.collider.GetComponent<Dialogue>().conversation[stringNumber];
+        }
+        else
+        {
+            panel.SetActive(false);
+            stringNumber = 0;
+            convo = false;
+        }
+    }
+}
---
+++ P3/Vluchtelingen game/Scripts/LopenEnRennen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LopenEnRennen : MonoBehaviour
+{
+
+    public int speed;
+    public Vector3 a;
+    public float hor;
+    public float ver;
+
+    // Lopen
+    public void FixedUpdate()
+    {
+        ver = Input.GetAxis("Vertical");
+        hor = Input.GetAxis("Horizontal");
+
+        a.z = ver;
+        a.x = hor;
+
+        transform.Translate(a * Time.deltaTime * speed);
+    }
+
+    //Hiermee kun je een water bottle van de grond oppakken en daardoor krijg je extra speed.
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Water") 
+        {
+            speed = 10;
+            col.gameObject.SetActive(false); //hierdoor verdwijnt het object als je het aangeraakt hebt.
+        }
+    }
+}
